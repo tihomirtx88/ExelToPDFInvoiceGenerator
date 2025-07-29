89aae235b94ddb197494eab3fe6f17dcc8138fc3
tihomirtx88 <tihomirtx88@gmail.com>
2025-07-29 14:10:53 +0300

    Adding table header and total price
    Adding table header and total price

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import glob
 from fpdf import FPDF, XPos, YPos
 from pathlib import Path
 
+from openpyxl.styles.builtins import styles
+
 filepaths = glob.glob("invoces/*.xlsx");
 
 for filepath in filepaths:
@@ -46,5 +48,24 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, text=str(row["amount_purchased"]), border=1);
         pdf.cell(w=30, h=8, text=str(row["price_per_unit"]), border=1);
         pdf.cell(w=30, h=8, text=str(row["total_price"]), border=1, new_x=XPos.LMARGIN, new_y=YPos.NEXT);
+
+    # Total sum
+    total_sum = df["total_price"].sum();
+    pdf.set_font(family="Times", size=10);
+    pdf.set_text_color(80, 80, 80);
+    pdf.cell(w=30, h=8, text="", border=1);
+    pdf.cell(w=70, h=8, text="", border=1);
+    pdf.cell(w=30, h=8, text="", border=1);
+    pdf.cell(w=30, h=8, text="", border=1);
+    pdf.cell(w=30, h=8, text=str(total_sum), border=1, new_x=XPos.LMARGIN, new_y=YPos.NEXT);
+
+    # Add total sum sentense
+    pdf.set_font(family="Times", size=10);
+    pdf.cell(w=30, h=8, text=f"The total price is {total_sum}", new_x=XPos.LMARGIN, new_y=YPos.NEXT);
+
+    # Company name and logo
+    pdf.set_font(family="Times", size=14, style="B");
+    pdf.cell(w=25, h=8, text=f"PythonHow ", new_x=XPos.LMARGIN, new_y=YPos.NEXT);
+    pdf.image("pythonhow.png", w=10);
 
     pdf.output(f"PDFS/{filename}.pdf");
